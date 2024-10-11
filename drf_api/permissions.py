d8a65b1f1e77c87fc5902536b900cd69069e2761
Florian Hass <f91hama@gmail.com>
2024-10-11 08:01:22 +0000

    Fix typo in permissions.py

--- drf_api/permissions.py
+++ drf_api/permissions.py
@@ -1,18 +1,8 @@
-from rest_framework import serializers
-from .models import Profile
+from rest_framework import permissions
 
 
-class ProfileSerializer(serializers.ModelSerializer):
-    owner = serializers.ReadOnlyField(source='owner.username')
-    is_owner = serializers.SerializerMethodField()
-
-    def get_is_owner(self, obj):
-        request = self.context['request']
-        return request.user == obj.owner
-
-    class Meta:
-        model = Profile
-        fields = [
-            'id', 'owner', 'created_at', 'updated_at', 'name',
-            'content', 'image', 'is_owner'
-        ]
+class IsOwnerOrReadOnly(permissions.BasePermission):
+  def has_object_permission(self, request, view, obj):
+    if request.method in permissions.SAFE_METHODS:
+      return True
+    return obj.owner == request.user
