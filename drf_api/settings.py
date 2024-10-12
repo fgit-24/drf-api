99507a5aafebdd8174d73a0e550f35f87aaf28bf
Florian Hass <f91hama@gmail.com>
2024-10-12 11:17:47 +0000

    feat: Configure CORS settings based on CLIENT_ORIGIN environment variable
    - Added conditional logic to set CORS_ALLOWED_ORIGINS from CLIENT_ORIGIN
      if it exists in the environment.
    - If CLIENT_ORIGIN is not set, allowed origins are restricted to
      gitpod.io subdomains using CORS_ALLOWED_ORIGIN_REGEXES.

--- drf_api/settings.py
+++ drf_api/settings.py
@@ -63,6 +63,7 @@ DEBUG = True
 ALLOWED_HOSTS = [
     '127.0.0.1',
     '8000-fgit24-drfapi-l4onqw41z7n.ws.codeinstitute-ide.net',
+    'MY-APP-Heroku.com'
 ]
 
 CSRF_TRUSTED_ORIGINS = ['https://8000-fgit24-drfapi-l4onqw41z7n.ws.codeinstitute-ide.net']
@@ -88,6 +89,7 @@ INSTALLED_APPS = [
     'allauth.account',
     'allauth.socialaccount',
     'dj_rest_auth.registration',
+    'corsheaders',
 
     'profiles',
     'posts',
@@ -97,6 +99,7 @@ INSTALLED_APPS = [
 ]
 SITE_ID = 1
 MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -105,6 +108,15 @@ MIDDLEWARE = [
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
+
+if 'CLIENT_ORIGIN' in os.environ:
+    CORS_ALLOWED_ORIGINS = [
+        os.environ.get('CLIENT_ORIGIN')
+    ]
+else:
+    CORS_ALLOWED_ORIGIN_REGEXES = [
+        r"^https://.*\.gitpod\.io$",
+    ]
 
 ROOT_URLCONF = 'drf_api.urls'
 
