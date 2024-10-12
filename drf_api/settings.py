e5d39dbff798e93a13b16a11a8c9b80d3f978df6
Florian Hass <f91hama@gmail.com>
2024-10-12 19:41:32 +0000

    chore(config): update CORS settings for Gitpod environments
    - Replaced fixed Gitpod CORS regex with dynamic extraction from `CLIENT_ORIGIN_DEV`.
    - CORS_ALLOWED_ORIGIN_REGEXES now supports both EU and US Gitpod clusters by dynamically extracting the base URL.
    - Enhanced flexibility for handling multiple Gitpod environments.

--- drf_api/settings.py
+++ drf_api/settings.py
@@ -13,6 +13,7 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 from pathlib import Path
 import os
 import dj_database_url
+import re
 
 if os.path.exists('env.py'):
     import env
@@ -64,7 +65,7 @@ DEBUG = 'DEV' in os.environ
 ALLOWED_HOSTS = [
     '127.0.0.1',
     '8000-fgit24-drfapi-l4onqw41z7n.ws.codeinstitute-ide.net',
-    'pp5-api-f-176f17fcc3bf.herokuapp.com'
+    os.environ.get('ALLOWED_HOST'),
 ]
 
 CSRF_TRUSTED_ORIGINS = [
@@ -112,13 +113,10 @@ MIDDLEWARE = [
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 
-if 'CLIENT_ORIGIN' in os.environ:
-    CORS_ALLOWED_ORIGINS = [
-        os.environ.get('CLIENT_ORIGIN')
-    ]
-else:
+if 'CLIENT_ORIGIN_DEV' in os.environ:
+    extracted_url = re.match(r'^.+-', os.environ.get('CLIENT_ORIGIN_DEV', ''), re.IGNORECASE).group(0)
     CORS_ALLOWED_ORIGIN_REGEXES = [
-        r"^https://.*\.gitpod\.io$",
+        rf"{extracted_url}(eu|us)\d+\w\.gitpod\.io$",
     ]
 
 CORS_ALLOW_CREDENTIALS = True
