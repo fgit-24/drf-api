f6b67ff1415fe490db6867c3a417442f9b3739ee
Florian Hass <f91hama@gmail.com>
2024-10-14 18:33:42 +0000

    Add album models, views, and serializer

--- albums/views.py
+++ albums/views.py
@@ -1,3 +1,33 @@
-from django.shortcuts import render
+from rest_framework import generics, permissions
+from .models import Album
+from .serializers import AlbumSerializer
 
-# Create your views here.
+
+class AlbumList(generics.ListCreateAPIView):
+    """
+    Returns a list of all albums.
+    An album can be created by an authenticated user.
+    Only the owner can list their albums.
+    """
+    serializer_class = AlbumSerializer
+    permission_classes = [permissions.IsAuthenticated]
+
+    def get_queryset(self):
+        user = self.request.user
+        return Album.objects.filter(owner=user)
+
+    def perform_create(self, serializer):
+        serializer.save(owner=self.request.user)
+
+
+class AlbumDetail(generics.RetrieveUpdateDestroyAPIView):
+    """
+    View a single album if you own it.
+    Update or delete it if you own it.
+    """
+    serializer_class = AlbumSerializer
+    permission_classes = [permissions.IsAuthenticated]
+
+    def get_queryset(self):
+        user = self.request.user
+        return Album.objects.filter(owner=user)
