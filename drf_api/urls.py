2be295a059c3080d88930da45005aef2c4958e50
Florian Hass <f91hama@gmail.com>
2024-10-17 08:04:21 +0000

    Add followers url

--- drf_api/urls.py
+++ drf_api/urls.py
@@ -28,7 +28,7 @@ urlpatterns = [
         'dj-rest-auth/registration/', include('dj_rest_auth.registration.urls')
     ),
     path('', include('profiles.urls')),
-    path('', include('followers')),
+    path('', include('followers.urls')),
     path('', include('posts.urls')),
     path('', include('comments.urls')),
     path('', include('likes.urls')),
